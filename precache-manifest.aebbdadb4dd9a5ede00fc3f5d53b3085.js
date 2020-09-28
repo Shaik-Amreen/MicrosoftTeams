e675b0b6aaf796045c8d30facf601cd6905c677b
Shaik-Amreen <amreenshaik40@gmail.com>
2020-09-28 16:22:19 +0530

    Updates

---
+++ precache-manifest.aebbdadb4dd9a5ede00fc3f5d53b3085.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40aec93ecd57a83dda3650f6d44846a8",
+    "url": "/MicrosoftTeams/index.html"
+  },
+  {
+    "revision": "d8b494ff29c7bd99b3f5",
+    "url": "/MicrosoftTeams/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "d09edf815dbd8be9865c",
+    "url": "/MicrosoftTeams/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8b494ff29c7bd99b3f5",
+    "url": "/MicrosoftTeams/static/js/2.268e2092.chunk.js"
+  },
+  {
+    "revision": "2f97bfe5b7c1c72b23be28739730a643",
+    "url": "/MicrosoftTeams/static/js/2.268e2092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d09edf815dbd8be9865c",
+    "url": "/MicrosoftTeams/static/js/main.f2fee025.chunk.js"
+  },
+  {
+    "revision": "42fe4e2055e2743b8e18",
+    "url": "/MicrosoftTeams/static/js/runtime-main.a1a98e56.js"
+  }
+]);
